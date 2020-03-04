e8fc31f76d879cec3d1b928e8c33c4bfe9437cf5
manuelatrotta <manuelat88@hotmail.it>
2020-03-04 19:20:32 +0100

    creazione tabella

--- index.php
+++ index.php
@@ -6,10 +6,27 @@ include 'database.php';
  <html lang="en" dir="ltr">
    <head>
      <meta charset="utf-8">
-     <title>php-hotel-crud</title>
      <link rel="stylesheet" href="dist/app.css">
+     <title>php-hotel-crud</title>
    </head>
    <body>
+     <table>
+       <thead>
+         <tr>
+           <td>ID</td>
+           <td>floor</td>
+           <td></td>
+           <td></td>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td></td>
+           <td></td>
+           <td></td>
+         </tr>
+       </tbody>
+     </table>
 
    </body>
  </html>
