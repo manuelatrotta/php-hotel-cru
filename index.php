ef60dae2097e323d88b299b38cb987788568f0b0
manuelatrotta <manuelat88@hotmail.it>
2020-03-04 19:16:17 +0100

    collegamento css

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 include 'database.php';
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>php-hotel-crud</title>
+     <link rel="stylesheet" href="dist/app.css">
+   </head>
+   <body>
+
+   </body>
+ </html>
