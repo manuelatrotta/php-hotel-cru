7bdc8431dfa15588b632b42b92ec2cc817e1fa64
manuelatrotta <manuelat88@hotmail.it>
2020-03-04 20:49:40 +0100

    inserimento header

---
+++ partials/header.php
@@ -0,0 +1,9 @@
+//pezzo html in comune
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="dist/app.css">
+  <title>php-hotel-crud</title>
+</head>
--- show/show.php
+++ show/show.php
@@ -1,1 +1,21 @@
-
+<?php
+include __DIR__ . '/../partials/header.php';
+?>
+  <body>
+    <div class="container">
+      <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <ul>
+                <?php include 'server.php' ?>
+                <li>ID: <?php echo $room['id']; ?></li>
+                <li>Floor: <?php echo $room['floor']; ?></li>
+                <li>Room number: <?php echo $room['room_number']; ?></li>
+                <li>Beds: <?php echo $room['beds']; ?></li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
