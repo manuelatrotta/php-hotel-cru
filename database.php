0e7c557b764b5337630297f2d70146b6bae1312d
manuelatrotta <manuelat88@hotmail.it>
2020-03-04 18:26:48 +0100

    inserimento connessione

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+  include 'env.php';
+  // connect
+  $conn = new mysqli($servername, $username, $password, $dbname);
+//check connection
+  if ($conn && $conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+    die();
+  }
+
+ ?>
